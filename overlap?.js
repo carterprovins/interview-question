f9e02d877e6f6f6d50e4b58a374a76f8fa65ce19
Carter D Provins <carterprovins@gmail.com>
2022-05-08 04:10:35 +0000

    removed excess if statement

--- overlap?.js
+++ overlap?.js
@@ -1,7 +1,3 @@
 function overlapCheck(rht1, top1, lft1, btm1, rht2, top2, lft2, btm2){
-    
-    if ((btm2<top1<top2 || btm1<top2<top1) && (lft1<lft2<rht1 || lft2<lft1<rht2)){
-        return true
-    }
-    return false
+    return (btm2<top1<top2 || btm1<top2<top1) && (lft1<lft2<rht1 || lft2<lft1<rht2)
 }
